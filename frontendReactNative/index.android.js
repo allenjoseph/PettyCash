59e5e8744032c9c4dec4a2f7fc61593a05fd2c9d
Allen Joseph <allen.joseph.v.a@gmail.com>
2016-01-06 23:05:38 -0500

    Update index.android.js to content pettyCash app

--- frontendReactNative/index.android.js
+++ frontendReactNative/index.android.js
@@ -1,52 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
 var {
-  AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   View,
+  WebView
 } = React;
 
-var frontendReactNative = React.createClass({
+var HEADER = '#3b5998';
+var BGWASH = 'rgba(255,255,255,0.8)';
+
+var DEFAULT_URL = 'http://localhost:3000';
+
+var PettyCashWebView = React.createClass({
+
+  getInitialState: function() {
+    return {
+      url: DEFAULT_URL,
+      forwardButtonEnabled: false,
+      loading: true,
+      scalesPageToFit: true,
+    };
+  },
+
+  inputText: '',
+
+  handleTextInputChange: function(event) {
+    this.inputText = event.nativeEvent.text;
+  },
+
   render: function() {
+    this.inputText = this.state.url;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={[styles.container]}>
+        <WebView
+          ref={WEBVIEW_REF}
+          automaticallyAdjustContentInsets={false}
+          style={styles.webView}
+          url={this.state.url}
+          javaScriptEnabledAndroid={true}
+          domStorageEnabledAndroid={true}
+          onNavigationStateChange={this.onNavigationStateChange}
+          onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
+          startInLoadingState={true}
+          scalesPageToFit={this.state.scalesPageToFit}
+        />
       </View>
     );
+  },
+
+  onShouldStartLoadWithRequest: function(event) {
+    // Implement any custom loading logic here, don't forget to return!
+    return true;
+  },
+
+  onNavigationStateChange: function(navState) {
+    this.setState({
+      forwardButtonEnabled: navState.canGoForward,
+      url: navState.url,
+      loading: navState.loading,
+      scalesPageToFit: true
+    });
   }
+
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: HEADER,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  webView: {
+    backgroundColor: BGWASH
+  }
 });
 
-AppRegistry.registerComponent('frontendReactNative', () => frontendReactNative);
+AppRegistry.registerComponent('PettyCashWebView', () => PettyCashWebView);
